cff48987f885d3a7a2c992d01b2d8ba421adb67a
Louis Narmour <louis@Louis-MacBook-Pro.local>
2021-06-01 07:39:55 -0500

    init

---
+++ src/main/java/com/random/App.java
@@ -0,0 +1,7 @@
+package com.random;
+
+public class App {
+    public static void main(String[] commandLineArguments) {
+        MicroServer server = new MicroServer(8088);
+    }
+
---
+++ src/main/java/com/random/Server.java
@@ -0,0 +1,23 @@
+package com.random;
+
+import java.util.Random
+import static spark.Spark.*;
+
+class Server {
+
+    public Random r = new Random();
+
+    Server(int serverPort) {
+        port(serverPort);
+        redirect.get("/", "/index.html");
+        path("/api", () -> {
+            get("/random", (req, res) -> processHttpRequest(req, res));
+        });
+    }
+
+    private String processHttpRequest(spark.Response res) {
+        res.type("application/json");
+        res.status(200);
+        return "{\"value\": \"" + r.nextInt(1000) + "\"}";
+    }
+}
